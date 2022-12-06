7434da8af844b464aa3673469d01e88f325a46e6
Junior Yono <jryono123@gmail.com>
2022-12-05 16:56:25 -0800

    Create ExampleTest.java

---
+++ src/test/ExampleTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+ 
+class ExampleTest {
+ 
+    @Test
+    void exampleTest() {
+    	assertTrue(true);
+    }
+    
+    @Test
+    void exampleTest2() {
+    	assertTrue(false);
+    }
+}
